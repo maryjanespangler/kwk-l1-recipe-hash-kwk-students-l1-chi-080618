9da9ba3db2ade245f0b3cc0c7b53ecac1e0928fb
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-09 19:29:29 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -12,7 +12,8 @@ perfect_10_recipe {:almond_flour => "2 cups"
 :olive_oil => "1/2 cup" 
 :agave => "1/4 cup"}
  return perfect_10_recipe
- 
+  end
+ end
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
